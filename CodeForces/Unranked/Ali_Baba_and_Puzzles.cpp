2b9cea0cb6a6edd3b3298c1ed6eb6948fafa1085
mrwhite031 <riasitur123@gmail.com>
2026-05-24 14:30:34 +0600

    Solved

---
+++ CodeForces/Unranked/Ali_Baba_and_Puzzles.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if ( (a + b) - c == d ) cout << "YES\n";      
+    else if ( (a - b) + c == d ) cout << "YES\n";     
+    else if ( a + (b * c) == d ) cout << "YES\n";     
+    else if ( a - (b * c) == d ) cout << "YES\n";     
+    else if ( (a * b) + c == d ) cout << "YES\n";     
+    else if ( (a * b) - c == d ) cout << "YES\n";     
+    else cout << "NO\n";
+
+    return 0;
+}
