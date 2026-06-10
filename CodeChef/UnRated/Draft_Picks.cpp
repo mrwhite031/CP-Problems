7fddbf87868f4cc8c5d8aab86bccd80033b97e26
mrwhite031 <riasitur123@gmail.com>
2026-06-10 21:25:09 +0600

    Solved

--- CodeChef/UnRated/Draft_Picks.cpp
+++ CodeChef/UnRated/Draft_Picks.cpp
@@ -1,30 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-int t;
-cin>>t;
-while(t--){
-int n,k;
-cin>>n>>k;
+int main()
+{
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      int n, k;
+      cin >> n >> k;
 
-  vector<long long>deck(n+1);
+      vector<long long> deck(n + 1);
 
-  int card = k;
+      int card = k;
 
-  while(card > 0){
-     for(int i = 1; i <= n && card > 0; i++){
-        deck[i] += card;
-        card--;
-     }
+      while (card > 0)
+      {
+         for (int i = 1; i <= n && card > 0; i++)
+         {
+            deck[i] += card;
+            card--;
+         }
 
-     for(int i = n; i >= 1 && card > 0; i++){
-        deck[i] += card;
-        card--;
-     }
-  }
-  cout<<*max_element(deck.begin()+1, deck.end())<<endl;
-
-
-}
+         for (int i = n; i >= 1 && card > 0; i--)
+         {
+            deck[i] += card;
+            card--;
+         }
+      }
+      int max = 0;
+      for (int i = 1; i < deck.size(); i++)
+      {
+         if (deck[i] > max)
+         {
+            max = deck[i];
+         }
+      }
+      cout << max << endl;
+   }
 }
